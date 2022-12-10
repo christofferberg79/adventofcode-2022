39ffbd6f053461956228764c2a818c3f056911ec
Christoffer Berg <christofferberg79@gmail.com>
2022-12-10 14:07:52 +0100

    Day 10, part 2

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -1,11 +1,35 @@
 package cberg.aoc2022
 
+import kotlin.math.abs
+
 class Day10(private val input: List<String>) {
     private var cycle = 0
     private var x = 1
-    private var sum = 0
 
     fun part1(): Int {
+        var sum = 0
+        execute {
+            if (++cycle in 20..220 step 40) {
+                sum += cycle * x
+            }
+        }
+        return sum
+    }
+
+    fun part2(): List<String> {
+        val image = mutableListOf("")
+        execute {
+            val output = if (abs(cycle++ % 40 - x) <= 1) "#" else "."
+            if (image.last().length == 40) {
+                image.add(output)
+            } else {
+                image.add(image.removeLast() + output)
+            }
+        }
+        return image
+    }
+
+    private fun execute(tick: () -> Unit) {
         for (line in input) {
             tick()
             if (line != "noop") {
@@ -13,14 +37,6 @@ class Day10(private val input: List<String>) {
                 x += line.split(" ").last().toInt()
             }
         }
-        return sum
     }
 
-    fun part2() = 0
-
-    private fun tick() {
-        if (++cycle in 20..220 step 40) {
-            sum += cycle * x
-        }
-    }
 }
